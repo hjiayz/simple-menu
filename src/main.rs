d5ffaf0fdc726635c2824b1b1f494edc15e77f2f
hjiayz <hjiayz@163.com>
2019-11-15 09:23:35 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@
 extern crate iui;
 use iui::controls::{Button, VerticalBox};
 use iui::prelude::*;
-use std::ffi::OsString;
 use std::process::Command;
 
 fn get_cfg_dir() -> Option<String> {
@@ -39,37 +38,32 @@ fn load_scripts() -> Option<Vec<(String, String, String)>> {
         .ok()
 }
 
-fn save_log(output_msg: &[u8], error_msg: &[u8]) -> Option<()> {
+fn load_title() -> Option<String> {
     use std::fs;
-    use std::io::Write;
-    let dir = get_cfg_dir()?;
-    let mut output = std::path::PathBuf::from(dir);
-    let mut error = output.clone();
-    output.push("output.log");
-    error.push("error.log");
-    let mut f = fs::OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open(&output)
-        .ok()?;
-    f.write_all(output_msg).ok()?;
-    f.flush().ok()?;
-    fs::OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open(&error)
-        .ok()?;
-    f.write_all(error_msg).ok()?;
-    f.flush().ok()?;
-    Some(())
+    use std::io::Read;
+    use std::path::PathBuf;
+
+    let mut path = PathBuf::from(get_cfg_dir()?);
+    path.push("settings");
+    path.set_extension("toml");
+    let mut file = fs::File::open(path).ok()?;
+    let mut buf = String::new();
+    file.read_to_string(&mut buf).ok()?;
+    let values: toml::Value = toml::from_str(&buf).ok()?;
+    let table = values.as_table()?;
+    Some(table.get("title")?.as_str()?.to_owned())
 }
 
 fn main() {
-    let scripts = load_scripts().unwrap_or_default();
+    let title = load_title().unwrap_or("menu".to_owned());
+
+    let mut scripts = load_scripts().unwrap_or_default();
+
+    scripts.sort_by_key(|s| s.1.to_owned());
 
     let ui = UI::init().expect("Couldn't initialize UI library");
 
-    let mut win = Window::new(&ui, "menu", 200, 400, WindowType::NoMenubar);
+    let mut win = Window::new(&ui, &title, 200, 400, WindowType::NoMenubar);
 
     let mut vbox = VerticalBox::new(&ui);
     vbox.set_padded(&ui, true);
@@ -99,27 +93,15 @@ fn main() {
                 #[cfg(windows)]
                 cmd.creation_flags(0x0800_0000);
 
-                if let Ok(out) = cmd.output() {
-                    if std::env::args_os().nth(1) == Some(OsString::from("debug")) {
-                        save_log(&out.stdout, &out.stderr);
-                    }
+                if let Err(_) = cmd.spawn() {
+                    println!("{} command failed to start", &command.join(" "))
                 }
-
                 ui.quit();
             }
         });
         vbox.append(&ui, button, LayoutStrategy::Compact);
     }
 
-    let mut quit_button = Button::new(&ui, "Quit");
-    quit_button.on_clicked(&ui, {
-        let ui = ui.clone();
-        move |_| {
-            ui.quit();
-        }
-    });
-
-    vbox.append(&ui, quit_button, LayoutStrategy::Compact);
     win.set_child(&ui, vbox);
     win.show(&ui);
     ui.main();
